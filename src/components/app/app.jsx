03af9eb0b8293b50ff461a1dfc590702885eb413
GolotinaNina <102403588+GolotinaNina@users.noreply.github.com>
2023-09-18 15:25:46 +0300

    fix

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,16 +8,25 @@ import AppHeader from "../appheader/appheader";
 import Main from "../main/main";
 
 
+
 class App extends React.Component {
     render() {
         return (
             <div>
                 <AppHeader />
+                <div className={styles.burgerZone}>
                 <Main />
+               
+      
+      </div>
                 
             </div>
         )
     }
 }
 
+
+
+  
+ 
 export default App; 
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,27 +2,31 @@ import React from 'react';
 import mainStyles from './main.module.css';
 import { useState } from 'react';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import PropType from  'prop-types';
+
 
 class Main extends React.Component {
   render() {
-    const [current, setCurrent] = React.useState('one');
+   
+   
     return (
+     
       <main className={mainStyles.main}>
+   
         <section className={mainStyles.section}>
           <h1 className={mainStyles.title}>Соберите бургер</h1>
-          
           <div style={{ display: 'flex' }}>
-            <Tab value="one" active={current === 'one'} onClick={setCurrent}>
-              One
-            </Tab>
-            <Tab value="two" active={current === 'two'} onClick={setCurrent}>
-              Two
-            </Tab>
-            <Tab value="three" active={current === 'three'} onClick={setCurrent}>
-              Three
-            </Tab>
-          </div>
-
+          <Tab>
+        One
+      </Tab>
+      <Tab>
+        Two
+      </Tab>
+      <Tab>
+        Three
+      </Tab>
+        </div>
+    
         </section>
 
       </main>
