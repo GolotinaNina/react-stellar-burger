a1031af1473c0b05bdf2fa169a787154a894b32b
GolotinaNina <Golotina.nina1990@gmail.com>
2023-10-11 15:50:57 +0300

    Add Scroll by Category AND add files type in gitignore

--- src/components/burger-ingredients/BurgerIngredients.jsx
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -6,36 +6,57 @@ import { data } from '../../utils/data';
 
 
 function BurgerIngredients() {
-    const [current, setCurrent] = useState('bun')
+
+    const [currentTab, setCurrentTab] = useState("bun");
+
+    const onTabClick = (tab) => {
+        setCurrentTab(tab);
+        const ref = document.getElementById(tab);
+        if (ref) {
+        ref.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+    let typeReady = '';
 
     return (
         <section className={styles.section}>
             <h1 className='text text_type_main-large pt-10 pb-5'>Соберите бургер</h1>
             <div className={`${styles['burger-menu']} pb-10`}>
-                <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+                <Tab value="bun" active={currentTab === 'bun'} onClick={() => onTabClick("bun")}>
                     Булки
                 </Tab>
-                <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+                <Tab value="sauce" active={currentTab === 'sauce'} onClick={() => onTabClick("sauce")}>
                     Соусы
                 </Tab>
-                <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+                <Tab value="main" active={currentTab === 'main'} onClick={() => onTabClick("main")}>
                     Начинки
                 </Tab>
             </div>
-
             <div className={`${styles['burger-card']} custom-scroll`}>
-                <IngredientCards title="Булки"
-                    cardsArr={data.filter((el) => {
-                        return el.type === "bun";
-                    })} />
-                <IngredientCards title="Соусы"
-                    cardsArr={data.filter((el) => {
-                        return el.type === "sauce";
-                    })} />
-                <IngredientCards title="Начинки"
-                    cardsArr={data.filter((el) => {
-                        return el.type === "main";
-                    })} />
+                {["bun", "sauce", "main"].map((tab) => (
+                <div key={tab} id={tab}>
+                    <h2 className="text text_type_main-medium mt-10 mb-6">
+                    {tab === "bun" ? "Булки" : tab === "sauce" ? "Соусы" : "Начинка"}
+                    </h2>
+                    <ul className={`${styles["card-list"]} pl-4 pr-4`}>
+                    {data.map((item) => {
+                        if (item.type === tab && typeReady != item.type) {
+                            typeReady = item.type
+                            return (
+                                <IngredientCards
+                                cardsArr={data.filter((el)=> {
+                                    if(el.type === item.type){
+                                        return el.type
+                                    }
+                                })} />
+                            );
+                        }
+                        return null;
+                    })}
+                    </ul>
+                </div>
+                ))}
             </div>
         </section>
     );
