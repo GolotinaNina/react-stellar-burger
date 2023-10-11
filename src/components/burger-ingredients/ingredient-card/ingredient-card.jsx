10cca3da930b5141e88c335a9d29e8f77c07cedb
GolotinaNina <Golotina.nina1990@gmail.com>
2023-10-11 17:52:55 +0300

    fix bug (added element IngredientCard)

--- src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
+++ src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './ingredient-card.module.css';
-import  ingredientPropType from '../../../utils/prop-types';
+import ingredientPropType from '../../../utils/prop-types';
 import Modal from '../../modal/Modal';
 import IngredientDetails from '../../ingredient-details/IngredientDetails';
 
-function IngredientCard(item) {
+
+function IngredientCard({el}) {
   const [count, setCount] = useState(0);
-
   const [modalOpen, setModalOpen] = useState(false);
-
+  
   const handleModalOpen = () => {
     setModalOpen(true);
   }
@@ -20,27 +20,27 @@ function IngredientCard(item) {
       return (prev += 1);
     });
   };
-
+  
   return (
-    <div className={styles.card} onClick={handleClick} >
+    <div className={styles.card} onClick={handleClick}>
       {count > 0 && <Counter count={count} size="default" />}
-      <img className={styles.image} src={item.image} alt={item.name} onClick={handleModalOpen} />
+      <img className={styles.image} src={el.image} alt={el.name} onClick={handleModalOpen} />
 
       {modalOpen && (
         <Modal onClose={() => setModalOpen(false)}>
-          <IngredientDetails  item={item}/>
+          <IngredientDetails  el={el}/>
         </Modal>
       )}
       <div className={styles.price}>
-        <span className='text text_type_digits-default pb-1'>{item.price}</span>
+        <span className='text text_type_digits-default pb-1'>{el.price}</span>
         <CurrencyIcon type="primary" />
       </div>
-      <div className={`${styles['name-card']} text text_type_main-default`}>{item.name}</div>
+      <div className={`${styles['name-card']} text text_type_main-default`}>{el.name}</div>
     </div>
   );
 }
 
 IngredientCard.propTypes = {
-  item: ingredientPropType.isRequired,
+  el: ingredientPropType,
 };
 export default IngredientCard;
