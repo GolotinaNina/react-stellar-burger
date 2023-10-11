e45de9ec8ee63870ee649ad21873de33183d6ffd
GolotinaNina <Golotina.nina1990@gmail.com>
2023-10-11 17:26:08 +0300

    fix "ingridients" by "ingredients"

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,11 +5,12 @@ import BurgerIngredients from '../burger-ingredients/BurgerIngredients';
 import BurgerConstructor from '../burger-constructor/BurgerConstructor';
 import React, { useEffect, useState } from "react";
 
-function App() {
 
+function App() {
   const [data, setData] = useState([]);
 
   const BaseURL = 'https://norma.nomoreparties.space/api/ingredients ';
+
   useEffect(() => {
     fetch(BaseURL)
       .then((res) => {
@@ -26,11 +27,12 @@ function App() {
       });
   }, [BaseURL]);
 
+
   return (
     <div className={styles.app}>
       <AppHeader />
       <main className={styles['app-body']}>
-        <BurgerIngredients ingridients = {data} />
+        <BurgerIngredients ingredients = {data}/>
         <BurgerConstructor burgerArr={data} />
       </main>
     </div>
